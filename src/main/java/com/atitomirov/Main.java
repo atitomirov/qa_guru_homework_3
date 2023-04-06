366b35ce7dc6b46a5236e3cab91f0bb2597c5f0a
Alexey Titomirov <al.titomirov@gmail.com>
2023-04-06 15:33:52 +0300

    added getMessage() method

--- src/main/java/com/atitomirov/Main.java
+++ src/main/java/com/atitomirov/Main.java
@@ -3,6 +3,10 @@ package com.atitomirov;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println(getMessage());
+    }
+
+    private static String getMessage() {
+        return "Hello qa.guru";
     }
 }
