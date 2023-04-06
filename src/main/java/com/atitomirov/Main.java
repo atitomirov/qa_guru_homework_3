da9aa5dcda5c5ceb78a05408c481d469fca4c3e0
Alexey Titomirov <129100721+atitomirov@users.noreply.github.com>
2023-04-06 15:32:40 +0300

    Update Main.java

--- src/main/java/com/atitomirov/Main.java
+++ src/main/java/com/atitomirov/Main.java
@@ -1,8 +1,11 @@
 package com.atitomirov;
 
 public class Main {
+    
+    private static String msg = "Hello world!!!!!";
+    
     public static void main(String[] args) {
 
-        System.out.println("Hello world!!!!!");
+        System.out.println(msg);
     }
 }
